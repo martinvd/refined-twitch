7dd75511aeffd11d0c5831b8193e9b237a72d466
Martin van Driel <martinvd@gmail.com>
2020-06-20 11:17:34 +0200

    Automatically collect channel points

--- channel-points.js
+++ channel-points.js
@@ -1,7 +1,55 @@
-function detectChannelPointsRewardButton() {
-  console.log('test');
-}
+(function() {
+  function logNothingToCollect() {
+    console.log('no channel points to collect.');
+  }
 
-setInterval(function() {
-  detectChannelPointsRewardButton();
-}, 5000);
+  function simulateClick(element) {
+    var clickEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+
+    element.dispatchEvent(clickEvent);
+  }
+
+  function collectChannelPoints(button) {
+    simulateClick(button);
+    console.log('points collected.');
+  }
+
+  function monitorChannelPoints() {
+    console.log('monitoring channel points...');
+
+    var interval = setInterval(function() {
+      console.log('checking channel points...');
+
+      var pointsSummary = document.querySelector('.community-points-summary');
+
+      if (!pointsSummary) {
+        logNothingToCollect();
+        return;
+      }
+
+      var buttons = pointsSummary.querySelectorAll('button');
+
+      if (buttons.length !== 2) {
+        logNothingToCollect();
+        return;
+      }
+
+      clearInterval(interval);
+
+      collectChannelPoints(buttons[buttons.length - 1]);
+      monitorChannelPoints();
+    }, 5000);
+  }
+
+  function onDOMContentLoaded() {
+    monitorChannelPoints();
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    onDOMContentLoaded();
+  });
+})();
